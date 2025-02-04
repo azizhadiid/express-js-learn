0a4afb0d12e9bf80a798292492140ef6dae2c037
azizhadiid <azizalhadiid88@gmail.com>
2025-02-04 08:00:48 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import url from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+
+app.get('/', (req, res) => {
+    // res.send('Hello World');
+    // res.json({
+    //     nama: "Aziz",
+    //     umur: 20
+    // });
+    // console.log(__dirname);
+    res.sendFile("./pages/index.html", {root: __dirname});
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile("./pages/about.html", {root: __dirname});
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile("./pages/contact.html", {root: __dirname});
+});
+
+app.use("*", (req, res) => {
+    res.status(404);
+    res.send("Halaman tidak ditemukan");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}); 
